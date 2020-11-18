4f44a5cd5b52b680ebff188aaa7bb91ff870ba4a
Niki Catapano <nsc9605@gmail.com>
2020-11-18 18:38:48 -0500

    updated html with buttons and proper CSS styling

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var today = moment();
+    $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
+
+    var hour = new Date().getHours();
+
+    
+})
