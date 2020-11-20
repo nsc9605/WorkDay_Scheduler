65bb341c51977944297fc2c0b69b9c9eb12c775d
Niki Catapano <nsc9605@gmail.com>
2020-11-19 23:25:27 -0500

    functioning if/else-if/else statement to document past/present/future tasks

--- Assets/script.js
+++ Assets/script.js
@@ -1,55 +1,59 @@
-const rows = document.getElementsByClassName("row");
-const hour = document.querySelector(".hour");
-// const hourPast = document.querySelector(".past");
-// const hourPresent = document.querySelector(".present");
-// const hourFuture = document.querySelector(".future");
-// const timeBlock = document.querySelector(".time-block");
+// Declaration of elements/classes
+const rows = $(".row");
+const hours = $(".hour");
+const past = $(".past");
+const present = $(".present");
+const future = $(".future");
+const timeBlock = $(".time-block");
+const saveButton = $(".saveBtn");
 
 // Set current time in header when application is opened
 $(document).ready(function () {
     var today = moment();
     $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
 
-    var hour = today.hours();
-    console.log(hours);
-    
+    var hours = today.hours();
+    console.log(hours);  
 })
+
+
 // set variable for current hour for color coding purposes
 var currentHour = moment().format('H');
 console.log(currentHour);
 
-// function setColor(color) {}
-var hourPast = hourPast < currentHour;
-var hourPresent = currentHour;
-var hourFuture = hourFuture > currentHour;
+let index = 0;
 
-let i = 0;
+console.log(timeBlock);
+
+// function to change color of textarea based on current time of the day
+timeBlock.each(function(index, value) {
+    console.log(`div${index}: ${this.id}`);
+
+    // for (index = 0; index <= timeBlock.length; index++) {
+        // console.log(index)
+        var id = "hour" + index;
+        // var timeBlock = $(id)
+        // console.log(index)
+        // var id = "hour" + index;
+        if (index < currentHour) {
+            $(this).addClass("past");
+        }
+        else if (index > currentHour) {
+            $(this).addClass("present");
+        }
+        else (index == currentHour) 
+            $(this).addClass("future");
+}       
+)
+// save to local storage
+$("button").on("click", save)
+
+function save() {
+    console.log($(this).siblings(textarea).val());
+localStorage.setItem("text", JSON.stringify(text));
+}
 
 
-// for loop for hours in the current work day
-for (let i = 1; i <= 24; i++) {
-    // console.log(i)
-    var id = ".hour" + i;
-    var timeBlock = $(id)
-    if (i < currentHour) {
-        timeBlock.addClass(hourPast)
-    }
-    else if (i > currentHour) {
-        timeBlock.addClass(hourFuture)
-    }
-    else (i == currentHour) 
-        timeBlock.addClass(hourPresent)
-    }
+// add event listeners
 
-
-// for (let hour = 0; hour < 24; hour++) {
-//     const past = isPast(hour)
-// }
-// }
-
-
-// function to test the hour
-
-// if hour < now = future 
-
-// 3 if / else arguments 
+// Function to save typed information
